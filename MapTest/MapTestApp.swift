65bd6de1569cb93c88768a1e8a9f2c940ab5994b
Gaultier <gmoraillon@exomind.fr>
2023-11-24 15:26:43 +0100

    Initial Commit

---
+++ MapTest/MapTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  MapTestApp.swift
+//  MapTest
+//
+//  Created by Gaultier on 24/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct MapTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
